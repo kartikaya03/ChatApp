ec4d99efa43527f416ee181d622b2f2cf27b84d1
Kartikaya Gupta <118833460+kartikaya03@users.noreply.github.com>
2025-01-08 14:05:10 +0530

    first git commit

---
+++ src/main/java/RealTimeChatBasedApplication/ChatBasedApplication.java
@@ -0,0 +1,13 @@
+package RealTimeChatBasedApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBasedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBasedApplication.class, args);
+	}
+
+}
